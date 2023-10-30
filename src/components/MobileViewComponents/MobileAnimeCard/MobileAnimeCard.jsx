ca91f3b90962646f67df7d8f359f44a50a96ad75
bladeeGenius <darkyt9@gmail.com>
2023-10-30 03:44:49 +0300

    add: popup for mobile

--- src/components/MobileViewComponents/MobileAnimeCard/MobileAnimeCard.jsx
+++ src/components/MobileViewComponents/MobileAnimeCard/MobileAnimeCard.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PopupContext } from '../../../providers/PopupProvider'
 
+import { BsFolderPlus } from 'react-icons/bs'
 import styles from './MobileAnimeCard.module.css'
 
 const MobileAnimeCard = ({ animeInfo }) => {
-    console.log(animeInfo)
+    const {setIsOpenPopup, setAnimeTitle} = useContext(PopupContext)
+    
   return (
     <div className={styles.cardWrapper}>
+        <BsFolderPlus
+            style={{color: "yellow", position:"absolute", width: "24px", height: "24px"}}
+            onClick={()=>{setIsOpenPopup(true); setAnimeTitle(animeInfo.title)}}
+        />
         <img src={animeInfo.poster} alt="Anime Poster" className={styles.cardPoster} />
-
         <div className={styles.cardInfo}>
             <h2 className={styles.cardTitle}>
                 {animeInfo.title}
